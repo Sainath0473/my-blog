abc1af56463ae0c2cfcb40c9c1aacd18a3d2858e
Sainath0473 <sainathreddyanumasu0473@gmail.com>
2024-07-08 12:44:21 -0400

    Add files via upload

---
+++ Travel_blog/js/javaScrpit.js
@@ -0,0 +1,8 @@
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        alert("Form submitted!");
+    });
+});
